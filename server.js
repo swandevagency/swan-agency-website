6cf369af3dcd1afb89b5069b9db23199e75ef449
mehraeen <a.mozhdi@gmail.com>
2022-02-16 15:02:07 +0330

    a bit cleaning

--- server.js
+++ server.js
@@ -10,31 +10,6 @@ mongoose.connect('mongodb://localhost:27017/swan-website',{useNewUrlParser : tru
     console.log('database is connected')
 })
 
-// require('./models/index')
-
-// const test = new mongoose.model('EnOurWorkContent')({
-//     clientName : 'gubrali',
-//     description : 'gubralitiz'
-// })
-// test.save()
-// const test2 = new mongoose.model('FaOurWorkContent')({
-//     clientName : 'گوبرلی',
-//     description : 'گوبرلی'
-// })
-// test2.save()
-
-// const test3 = new mongoose.model('OurWork')({
-//     projectName : 'project',
-//     for : '61f69c483b2d194036ff4e9e',
-//     clientImage : 'asklhfdsakgh',
-//     backgroundImage : 'ajsfhadgkhakgjksgjad',
-//     images : ['fkalsjkalsgjd','fhdaskjfdsaklgsaklg'],
-//     ourWorkContent : {
-//         en : test._id,
-//         fa : test2._id
-//     }
-// })
-// test3.save()
 
 app.use(express.json())
 
