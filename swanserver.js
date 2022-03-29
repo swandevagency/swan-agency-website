700b894b558499051d871a41794bda0476bc9b45
mehraeen <a.mozhdi@gmail.com>
2022-03-29 11:16:35 +0430

    port changed to 5000

--- swanserver.js
+++ swanserver.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 5000
 const cors = require('cors')
 
 mongoose.connect('mongodb://127.0.0.1:27017/swan-website',{useNewUrlParser : true})
